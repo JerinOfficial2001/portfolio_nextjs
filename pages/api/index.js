44a2dcd4e7c65cef93a365dbb8af1352e4313997
jerin.t <jerin.t@iprotecs.net>
2024-04-04 15:11:34 +0530

    totp

---
+++ pages/api/index.js
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import speakeasy from "speakeasy";
+
+export default function handler(req, res) {
+  const method = req.method;
+  switch (method) {
+    case "GET":
+      try {
+        const url = speakeasy.generateSecret({ length: 20 });
+
+        res.json({ status: "ok", data: url.otpauth_url, key: url.base32 });
+      } catch (error) {
+        console.log(error);
+      }
+      break;
+    case "POST":
+      try {
+        const { otp, key } = req.body;
+        try {
+          const verified = speakeasy.totp.verify({
+            secret: key,
+            encoding: "base32",
+            token: otp,
+          });
+          res.send({
+            status: "ok",
+            isVerified: verified,
+          });
+          console.log(QRkey, "PSOT");
+        } catch (error) {
+          console.log(error);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      break;
+    default:
+      break;
+  }
+}
--- pages/totp.js
+++ pages/totp.js
@@ -1,42 +1,25 @@
 // pages/index.js
 
+import axios from "axios";
+import QRCode from "qrcode.react";
 import { useState } from "react";
-import speakeasy from "speakeasy";
-import QRCode from "qrcode";
-
 export default function TOTP() {
   const [secret, setSecret] = useState("");
-  const [qrCodeUrl, setQrCodeUrl] = useState("");
   const [verified, setVerified] = useState(false);
   const [userInput, setUserInput] = useState("");
-  const SecretKey = "SecretKey";
-  const key = "LJOWEKCBJ42SUOKJOFSTY3JWJM7F2KBR";
-  const generateSecret = () => {
-    const secret = speakeasy.generateSecret({ length: 20 });
-    setSecret(secret.base32);
+  const [SecretKey, setSecretKey] = useState("");
 
-    // Generate QR code URL
-
-    QRCode.toDataURL(
-      `otpauth://totp/${SecretKey}?secret=${key}`,
-      (err, dataUrl) => {
-        if (err) {
-          console.error("Error generating QR code:", err);
-          return;
-        }
-        setQrCodeUrl(dataUrl);
-      }
-    );
+  const generateSecret = async () => {
+    const { data } = await axios.get("/api");
+    setSecret(data.data);
+    setSecretKey(data.key);
   };
-  const verifyToken = () => {
-    const verified = speakeasy.totp.verify({
-      secret: secret,
-      encoding: "base32",
-      token: userInput,
-      window: 2,
+  const verifyToken = async () => {
+    const { data } = await axios.post("/api", {
+      otp: userInput,
+      key: SecretKey,
     });
-    console.log(verified, "function");
-    setVerified(verified);
+    console.log(data);
   };
 
   return (
@@ -45,7 +28,8 @@ export default function TOTP() {
       {secret ? (
         <>
           <p>Scan the QR code with Google Authenticator:</p>
-          <img src={qrCodeUrl} alt="QR Code" />
+          {/* <img src={qrCodeUrl} alt="QR Code" /> */}
+          <QRCode style={{ marginLeft: 50 }} value={secret} />
           <p>Enter the verification code from Google Authenticator:</p>
           <input
             type="text"
