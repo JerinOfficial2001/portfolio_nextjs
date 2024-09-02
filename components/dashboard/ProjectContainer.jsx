a99807c7060e41619d70c33028bf0a8b086c99e7
jerin.t <jerin.t@iprotecs.net>
2024-09-02 18:57:35 +0530

    Show more error fixed

--- components/dashboard/ProjectContainer.jsx
+++ components/dashboard/ProjectContainer.jsx
@@ -37,7 +37,7 @@ export default function ProjectContainer({
     } else {
       setdisplayProjects([]);
     }
-  }, [projects?.length, projects]);
+  }, [projects?.length, projects, visibleCount]);
 
   return (
     <div className="w-[100%] flex flex-col items-center justify-center gap-5 ">
